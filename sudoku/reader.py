2f9d8cc972076a28e1fd0677f03f262cc30215df
Brandon Clement <17510591+bclement97@users.noreply.github.com>
2020-12-19 22:15:40 -0800

    Convert reader.read_img to pure function

--- sudoku/reader.py
+++ sudoku/reader.py
@@ -1,8 +1,5 @@
 import cv2 as cv
 
-img = None
-
 
 def read_img(img_src: str):
-    global img
-    img = cv.imread(img_src, cv.IMREAD_GRAYSCALE)
+    return cv.imread(img_src, cv.IMREAD_GRAYSCALE)
