2b395910fc344473e1624278c44698d306dca51c
Brandon Clement <17510591+bclement97@users.noreply.github.com>
2020-12-28 21:30:53 -0800

    Correct row/col arg positions and box indexing calculations

--- sudoku/grid.py
+++ sudoku/grid.py
@@ -11,37 +11,37 @@ class Grid:
         self._M = (src.reshape(Grid.SIZE) if src is not None
                    else np.zeros(Grid.SIZE))
 
-    def col(self, i: int) -> np.ndarray:
-        return self._M[:, i]
-
     def row(self, i: int) -> np.ndarray:
         return self._M[i, :]
 
+    def col(self, i: int) -> np.ndarray:
+        return self._M[:, i]
+
     def box(self, i: int) -> np.ndarray:
         # Since NumPy is row major, so are the boxes
-        col_start = i // self.BOX_DIM
-        col_end = col_start + self.BOX_DIM
-        row_start = i % self.BOX_DIM
+        row_start = self.BOX_DIM * (i // self.BOX_DIM)
         row_end = row_start + self.BOX_DIM
-        return self._M[col_start:col_end, row_start:row_end]
+        col_start = self.BOX_DIM * (i % self.BOX_DIM)
+        col_end = col_start + self.BOX_DIM
+        return self._M[row_start:row_end, col_start:col_end]
 
-    def get(self, col: int, row: int) -> int:
-        return self._M[col, row]
+    def get(self, row: int, col: int) -> int:
+        return self._M[row, col]
 
-    def set(self, col: int, row: int, value: int) -> bool:
-        self._M[col, row] = value
-        return self.validate_cell(col, row)
+    def set(self, row: int, col: int, value: int) -> bool:
+        self._M[row, col] = value
+        return self.validate_cell(row, col)
 
-    def clear(self, col: int, row: int) -> None:
-        self._M[col, row] = Grid.EMPTY_CELL
+    def clear(self, row: int, col: int) -> None:
+        self._M[row, col] = Grid.EMPTY_CELL
 
-    def validate_cell(self, col: int, row: int) -> bool:
+    def validate_cell(self, row: int, col: int) -> bool:
         def validate_group(cells: np.ndarray) -> bool:
             elems = cells[cells != Grid.EMPTY_CELL]
             return elems.size == np.unique(elems).size
 
         box = self.BOX_DIM * (row // self.BOX_DIM) + col // self.BOX_DIM
-        valid_col = validate_group(self.col(col))
         valid_row = validate_group(self.row(row))
+        valid_col = validate_group(self.col(col))
         valid_box = validate_group(self.box(box))
-        return valid_col and valid_row and valid_box
+        return valid_row and valid_col and valid_box
